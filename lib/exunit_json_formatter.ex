edf4153fc426f07bf5a3d70e392a1fbb2c877745
Jae Bach Hardie <jhardie@findmypast.com>
2017-07-03 09:47:04 +0100

    Initial commit after mix new

---
+++ lib/exunit_json_formatter.ex
@@ -0,0 +1,18 @@
+defmodule ExunitJsonFormatter do
+  @moduledoc """
+  Documentation for ExunitJsonFormatter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExunitJsonFormatter.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exunit_json_formatter_test.exs
@@ -0,0 +1,8 @@
+defmodule ExunitJsonFormatterTest do
+  use ExUnit.Case
+  doctest ExunitJsonFormatter
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
