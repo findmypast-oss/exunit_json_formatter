34d019e3ab5288c5d8c4c3ebf99cc7c7e36aa311
Jae Bach Hardie <jhardie@findmypast.com>
2017-07-04 12:21:28 +0100

    Added package details

--- mix.exs
+++ mix.exs
@@ -4,29 +4,26 @@ defmodule ExUnitJsonFormatter.Mixfile do
   def project do
     [app: :exunit_json_formatter,
      version: "0.1.0",
+     description: "ExUnit formatter that outputs a stream of JSON objects",
+     package: package(),
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  def package do
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     maintainers: ["Jae Bach Hardie"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/findmypast-oss/exunit_json_formatter"},
+     source_url: "https://github.com/findmypast-oss/exunit_json_formatter"]
+  end
+
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:poison, "~> 3.1"}]
   end
